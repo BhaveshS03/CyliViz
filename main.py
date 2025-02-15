7cc25f81c9e9777a311849ac0048fa9bbcd53e72
BhaveshS03 <bhaveshsonar003@gmail.com>
2025-02-15 11:54:43 +0530

    remove depricated code

--- main.py
+++ main.py
@@ -448,9 +448,9 @@ def run_standalone(app_manager: DashAppManager):
 
 
 if __name__ == "__main__":
-    exe = False if len(sys.argv)>1 else True
+    exe_flag = False if len(sys.argv)>1 else True
     app_manager = init_app()
-    if (exe):
+    if (exe_flag):
         run_standalone(app_manager)
     else:
         app_manager.app.run_server(debug=True)
